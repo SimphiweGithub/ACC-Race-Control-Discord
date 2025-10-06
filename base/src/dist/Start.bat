f03368153c92141ee807111d1608de0e270dd9e5
Simphiwe Khumalo <simphiwe0105@gmail.com>
2025-10-06 15:15:29 +0200

    Addedd the discord bot but now i need to add the jdk 25 jrm(bundle the jdk to the app) so it can run as  a standalone app as right now its dependant on having jdk on your pc

--- base/src/dist/Start.bat
+++ base/src/dist/Start.bat
@@ -2,5 +2,5 @@
 cd /d "%~dp0"
 
 REM Set Discord bot settings here (or via environment)
-set JAVA_OPTS=%JAVA_OPTS% "-Ddiscord.bot.token=MTQxOTYyMTc2OTcwNjM0NDQ3OA.GEhIIt.ohMgXUYnD2RZFz1MQMhIC3XsFKgCvx5NDUf99E" "-Ddiscord.channel.id=1418228156187414600"
+set JAVA_OPTS=%JAVA_OPTS% 
 start /b .\jre\bin\javaw.exe %JAVA_OPTS% -splash:data/loading.png -cp .\lib\* racecontrol.Main
